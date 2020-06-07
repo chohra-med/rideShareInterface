beb66bc2312f4197b0fc6a190aa4d3a34b014ac1
Chohra <fm_chohra@esi.dz>
2020-06-07 23:39:39 +0100

    eslint

--- src/views/home/home.style.js
+++ src/views/home/home.style.js
@@ -1,20 +1,21 @@
 import {Dimensions, StyleSheet} from 'react-native';
 import theme from '../../theme';
-const {height,width}=Dimensions.get("window");
+
+const {height, width} = Dimensions.get('window');
 export default StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
     alignItems: 'center',
-    backgroundColor:theme.PRIMARY_COLOR_DARK
+    backgroundColor: theme.PRIMARY_COLOR_DARK,
   },
   logo: {
-    width: width*0.2,
-    height: width*0.2,
+    width: width * 0.2,
+    height: width * 0.2,
   },
   message: {
     fontFamily: theme.FONT_FAMILY,
-    fontSize: width*0.12,
+    fontSize: width * 0.12,
     fontWeight: theme.WEIGHT_LIGHT,
     color: theme.WHITE,
     textAlign: 'center',
@@ -26,92 +27,89 @@ export default StyleSheet.create({
     color: theme.PRIMARY_COLOR,
     textAlign: 'center',
   },
-  logoAndText:{
+  logoAndText: {
     flexDirection: 'row',
-    width: width*0.6,
-    height:height*0.1,
-    justifyContent: "space-between",
-    alignItems:"center",
-    marginTop:40,
-    alignSelf:'flex-start',
-    marginLeft:40,
+    width: width * 0.6,
+    height: height * 0.1,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 40,
+    alignSelf: 'flex-start',
+    marginLeft: 40,
   },
-  welcomeText:{
-    marginTop:40,
-    width: width*0.88
+  welcomeText: {
+    marginTop: 40,
+    width: width * 0.88,
   },
-  heyThereText:{
+  heyThereText: {
     fontFamily: theme.FONT_FAMILY,
     fontSize: theme.SIZE_LARGE,
     fontWeight: theme.WEIGHT_MEDIUM,
     color: theme.WHITE,
     textAlign: 'left',
-
   },
-  typeText:{
-    marginTop:20,
-    fontFamily: theme.FONT_FAMILY,
-    fontSize: theme.SIZE_LARGE,
-    fontWeight: theme.WEIGHT_LIGHT,
-    color: theme.WHITE,
-    textAlign: 'left',
-
-  },
-  typeText:{
-    marginTop:20,
+  typeText: {
+    marginTop: 20,
     fontFamily: theme.FONT_FAMILY,
     fontSize: theme.SIZE_LARGE,
     fontWeight: theme.WEIGHT_LIGHT,
     color: theme.WHITE,
     textAlign: 'left',
   },
-  inputView:{
-    height:height*0.08,
-    width:width*0.8,
-    backgroundColor: "white",
-    flexDirection:'row',
-    alignItems:"center",
-    justifyContent: "space-around",
-    margin:40
+  typeText: {
+    marginTop: 20,
+    fontFamily: theme.FONT_FAMILY,
+    fontSize: theme.SIZE_LARGE,
+    fontWeight: theme.WEIGHT_LIGHT,
+    color: theme.WHITE,
+    textAlign: 'left',
   },
-  canSignText:{
-    marginTop:20,
+  inputView: {
+    height: height * 0.08,
+    width: width * 0.8,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    margin: 40,
+  },
+  canSignText: {
+    marginTop: 20,
     fontFamily: theme.FONT_FAMILY,
     fontSize: theme.SIZE_SMALL,
     fontWeight: theme.WEIGHT_LIGHT,
     color: theme.WHITE,
     textAlign: 'center',
   },
-  containerFacebookButtonStyle:{
-    borderRadius:10,
-    borderColor:theme.FACEBOOK,
-    height:height*0.06,
-    width:width*0.3
-  },containerGoogleStyle:{
-    borderRadius:5,
-    borderColor:theme.BLACK,
-    padding:5,
-    height:height*0.06,
-    width:width*0.3
-
-
+  containerFacebookButtonStyle: {
+    borderRadius: 10,
+    borderColor: theme.FACEBOOK,
+    height: height * 0.06,
+    width: width * 0.3,
   },
-  canSignView:{
-    position:'absolute',
-    bottom:height*0.1,
-    height:height*0.08,
-    alignItems:"center",
-    alignSelf:'center',
-    justifyContent:'center',
+  containerGoogleStyle: {
+    borderRadius: 5,
+    borderColor: theme.BLACK,
+    padding: 5,
+    height: height * 0.06,
+    width: width * 0.3,
   },
-  socialMediaSigning:{
-    position:'absolute',
-    height:height*0.1,
-    backgroundColor: "white",
-    bottom:0,
-    width:width,
-    flexDirection:'row',
-    alignItems:"center",
-    justifyContent: "space-around",
+  canSignView: {
+    position: 'absolute',
+    bottom: height * 0.1,
+    height: height * 0.08,
+    alignItems: 'center',
+    alignSelf: 'center',
+    justifyContent: 'center',
+  },
+  socialMediaSigning: {
+    position: 'absolute',
+    height: height * 0.1,
+    backgroundColor: 'white',
+    bottom: 0,
+    width,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
   },
 });
--- src/views/home/home.view.js
+++ src/views/home/home.view.js
@@ -1,119 +1,72 @@
 import React from 'react';
 import {SafeAreaView, View, Image, Text} from 'react-native';
+import {Input, SocialIcon, Button} from 'react-native-elements';
+import PhoneInput from 'react-native-phone-input';
 import styles from './home.style';
 import {LOGO} from '../../assets/icons';
 import {strings} from '../../locales/i18n';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import {Input, SocialIcon, Button} from 'react-native-elements';
-import PhoneInput from 'react-native-phone-input';
-import CountryPicker from 'react-native-country-picker-modal';
-
 import theme from '../../theme';
 
 export default class Component extends React.PureComponent {
-    constructor(props) {
-        super(props);
-        this.state = {
-            country: '',
-            cca2: 'US',
+  constructor(props) {
+    super(props);
+    this.state = {
+      country: '',
+    };
+  }
 
-        };
-        this.onPressFlag = this.onPressFlag.bind(this);
-        this.selectCountry = this.selectCountry.bind(this);
-    }
-    componentDidMount() {
-        this.setState({
-            pickerData: this.phone.getPickerData(),
-        });
-    }
-
-    onPressFlag() {
-        this.countryPicker.openModal();
-    }
-
-    selectCountry(country) {
-        alert(country.toString());
-        this.phone.selectCountry(country.cca2.toLowerCase());
-        this.setState({ cca2: country.cca2 });
-    }
-
-    render() {
-        return (
-            <SafeAreaView style={styles.container}>
-                <View style={styles.logoAndText}>
-                    <Image resizeMode="contain" style={styles.logo} source={LOGO}/>
-                    <Text style={styles.message}>{strings('home.pickmeUp')}</Text>
-                </View>
-                <View style={styles.welcomeText}>
-                    <Text style={styles.heyThereText}>{strings('home.heyThere')}</Text>
-                    <Text style={styles.typeText}>{strings('home.typePhoneNumber')}</Text>
-                </View>
-                <View>
-
-                    <Input
-                        containerStyle={styles.inputView}
-                        leftIcon={<View style={styles.container}>
-                            <PhoneInput
-                                ref={(ref) => {
-                                    this.phone = ref;
-                                }}
-                                onPressFlag={this.onPressFlag}
-                            />
-
-                            <CountryPicker
-                                ref={(ref) => {
-                                    this.countryPicker = ref;
-                                }}
-                                onChange={value => this.selectCountry(value)}
-                                translation="eng"
-                                cca2={this.state.cca2}
-                            >
-                                <View />
-                            </CountryPicker>
-                        </View>
-                        }
-                        placeholder="8812 213 821"
-                        label="Phone Number"
-                        keyboardType={'phone-pad'}
-                        labelStyle={{
-                            color: theme.BLACK,
-                            fontWeight: theme.WEIGHT_LIGHT,
-                        }}
-                        inputContainerStyle={{paddingLeft: 50}}
-                    >
-                    </Input>
-
-                </View>
-                <View style={styles.canSignView}>
-                    <Text style={styles.canSignText}>{strings('home.orCanSign')}</Text>
-                </View>
-                <View style={styles.socialMediaSigning}>
-
-                    <Button
-                        type="outline"
-                        icon={{
-                            name: 'logo-facebook',
-                            color: theme.FACEBOOK,
-                            type: 'ionicon',
-                            size: 22,
-                        }}
-                        titleStyle={{
-                            color: 'black',
-                        }}
-                        buttonStyle={styles.containerFacebookButtonStyle}
-                        title={strings('home.facebook')}
-                    />
-                    <SocialIcon
-                        type='google'
-                        light
-                        button
-                        title='Google'
-                        raised
-                        style={styles.containerGoogleStyle}
-                    />
-
-                </View>
-            </SafeAreaView>
-        );
-    }
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.logoAndText}>
+          <Image resizeMode="contain" style={styles.logo} source={LOGO} />
+          <Text style={styles.message}>{strings('home.pickmeUp')}</Text>
+        </View>
+        <View style={styles.welcomeText}>
+          <Text style={styles.heyThereText}>{strings('home.heyThere')}</Text>
+          <Text style={styles.typeText}>{strings('home.typePhoneNumber')}</Text>
+        </View>
+        <View>
+          <Input
+            containerStyle={styles.inputView}
+            leftIcon={<PhoneInput ref="phone" />}
+            placeholder="8812 213 821"
+            keyboardType="phone-pad"
+            labelStyle={{
+              color: theme.BLACK,
+              fontWeight: theme.WEIGHT_LIGHT,
+            }}
+            leftIconContainerStyle={styles.leftIcon}
+          />
+        </View>
+        <View style={styles.canSignView}>
+          <Text style={styles.canSignText}>{strings('home.orCanSign')}</Text>
+        </View>
+        <View style={styles.socialMediaSigning}>
+          <Button
+            type="outline"
+            icon={{
+              name: 'logo-facebook',
+              color: theme.FACEBOOK,
+              type: 'ionicon',
+              size: 22,
+            }}
+            titleStyle={{
+              color: 'black',
+            }}
+            buttonStyle={styles.containerFacebookButtonStyle}
+            title={strings('home.facebook')}
+          />
+          <SocialIcon
+            type="google"
+            light
+            button
+            title="Google"
+            raised
+            style={styles.containerGoogleStyle}
+          />
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
